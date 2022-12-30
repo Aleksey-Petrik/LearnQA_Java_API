574970470bc3a7f0bb11ab9b5ab076a9619777e3
Петрик Алексей <alexey.petric@gmail.com>
2022-12-30 19:07:35 +1000

    second commit

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,16 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void testHelloWorld(){
+        System.out.println("Hello from Aleksey");
+    }
+
+    @Test
+    public void testApi(){
+        Response response = RestAssured.get("https://playground.learnqa.ru/api/get_text").andReturn();
+        response.prettyPrint();
+    }
+}
