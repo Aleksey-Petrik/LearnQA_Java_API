fca93a98ac520580644b2b738f0239239cc17f86
Петрик Алексей <alexey.petric@gmail.com>
2023-01-08 14:10:33 +1000

    solution Ex5

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,16 +1,34 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     @Test
-    public void testHelloWorld(){
+    public void testHelloWorld() {
         System.out.println("Hello from Aleksey");
     }
 
     @Test
-    public void testApi(){
+    public void testApi() {
         Response response = RestAssured.get("https://playground.learnqa.ru/api/get_text").andReturn();
         response.prettyPrint();
+    }
+
+    /*
+    В рамках этой задачи нужно создать тест, который будет делать GET-запрос на адрес https://playground.learnqa.ru/api/get_json_homework
+    Полученный JSON необходимо распечатать и изучить. Мы увидим, что это данные с сообщениями и временем, когда они были написаны. Наша задача вывести текст второго сообщения.
+    Ответом должна быть ссылка на тест в вашем репозитории.
+     */
+    @Test
+    public void parseJson() {
+        JsonPath response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        response.prettyPrint();
+
+        System.out.println(response.getString("messages.find{it.message.contains('second')}"));
     }
 }
