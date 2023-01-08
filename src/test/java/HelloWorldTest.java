c4c4cd7e44cc2d391153e69fed22f0fdd529b0b7
Петрик Алексей <alexey.petric@gmail.com>
2023-01-08 15:46:47 +1000

    solution Ex8

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -78,4 +78,56 @@ public class HelloWorldTest {
         }
         System.out.printf("LastRedirect - %s, CountRedirects - %d%n", url, countRedirects);
     }
+
+    /*
+    Иногда API-метод выполняет такую долгую задачу, что за один HTTP-запрос от него нельзя сразу получить готовый ответ. Это может быть подсчет каких-то сложных вычислений или необходимость собрать информацию по разным источникам.
+    В этом случае на первый запрос API начинает выполнения задачи, а на последующие ЛИБО говорит, что задача еще не готова, ЛИБО выдает результат. Сегодня я предлагаю протестировать такой метод.
+    Сам API-метод находится по следующему URL: https://playground.learnqa.ru/ajax/api/longtime_job
+    Если мы вызываем его БЕЗ GET-параметра token, метод заводит новую задачу, а в ответ выдает нам JSON со следующими полями:
+    * seconds - количество секунд, через сколько задача будет выполнена
+    * token - тот самый токен, по которому можно получить результат выполнения нашей задачи
+
+    Если же вызвать API-метод, УКАЗАВ GET-параметром token, то мы получим следующий JSON:
+    * error - будет только в случае, если передать token, для которого не создавалась задача. В этом случае в ответе будет следующая надпись - No job linked to this token
+    * status - если задача еще не готова, будет надпись Job is NOT ready, если же готова - будет надпись Job is ready
+    * result - будет только в случае, если задача готова, это поле будет содержать результат
+
+    Наша задача - написать тест, который сделал бы следующее:
+    1) создавал задачу
+    2) делал один запрос с token ДО того, как задача готова, убеждался в правильности поля status
+    3) ждал нужное количество секунд с помощью функции Thread.sleep() - для этого надо сделать import time
+    4) делал бы один запрос c token ПОСЛЕ того, как задача готова, убеждался в правильности поля status и наличии поля result
+    Как всегда, код нашей программы выкладываем ссылкой на коммит.
+     */
+    @Test
+    public void longWork() throws InterruptedException {
+        Response response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .andReturn();
+        if (response.getStatusCode() == HttpStatus.SC_OK) {
+            JsonPath jsonPath = response.jsonPath();
+            int seconds = jsonPath.getInt("seconds");
+            String token = jsonPath.getString("token");
+
+            while (true) {
+                System.out.printf("Wait %d seconds%n", seconds);
+                Thread.sleep(seconds * 1000L);
+                response = RestAssured
+                        .given()
+                        .queryParam("token", token)
+                        .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                        .andReturn();
+                if (response.getStatusCode() == HttpStatus.SC_OK) {
+                    jsonPath = response.jsonPath();
+                    if (jsonPath.get("result") != null) {
+                        System.out.printf("Result - %d%n", jsonPath.getInt("result"));
+                        break;
+                    } else {
+                        System.out.println(jsonPath.getString("status"));
+                    }
+                }
+            }
+        }
+    }
 }
